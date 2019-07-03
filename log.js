6808040c5c9315160b76e4faa5ac8bfd63e76115
MattIPv4 <matthew@cowley.org.uk>
2019-07-03 21:55:47 +0100

    Add new log concept TODOs

---
+++ log.js
@@ -0,0 +1,15 @@
+/*
+ * TODO: Method to increase indent (use '>' to indicate level) of log messages
+ */
+
+/*
+ * TODO: Method to decrease indent level of log messages
+ */
+
+/*
+ * TODO: Method to log message, takes string and chalk color (callback essentially)
+ *  This should make use of the indent level as controlled above
+ */
+
+// TODO: Use this new logging method for all logging in script
+//  The aim being that this will make it visually easier to trace how methods are getting called in the stack
